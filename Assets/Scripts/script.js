6fea6630bab5cea70099eeca33414ea66e609790
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2023-11-16 00:21:01 -0500

    fixed and simplified logic tree, now working as expected

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -12,128 +12,20 @@ function generatePassword(length, special, lower, upper, num){
   const charsSpec = "\u0021\u0040\u0023\u0024\u0025\u0026\u0027\u0028\u0029\u002A\u002B\u002C\u002D\u002E\u002F\u003A\u003B\u003C\u003D\u003E\u003F\u0040\u005B\u005C\u005D\u005E\u005F\u0060\u007B\u007C\u007D\u007E";
 
   //Variable that updates with parameters.
-  const charSet = "";
-
-  
-
+  let charSet = "";
 
   //Sets password to blank
-  password = "";
+  let password = "";
 
-  // special == || lower == || upper == || num ==
+  if (special) charSet += charsSpec;
+  if (lower) charSet += charsLower;
+  if (upper) charSet += charsUpper;
+  if (num) charSet += charsNums;
+  
+  
+  console.log(charSet)
 
-  switch (charSet) {
-    case 1:
-      if (special === false && lower === false && upper === false && num === false) {
-        charSet = "";
-      }
-      break;
   
-    case 2:
-      if (special === false && lower === false && upper === false && num === true) {
-        charSet = charsNums;
-      }
-      break;
-  
-    case 3:
-      if (special === false && lower === false && upper === true && num === false) {
-        charSet = charsUpper;
-      }
-      break;
-  
-    case 4:
-      if (special === false && lower === false && upper === true && num === true) {
-        charSet = charsUpper + charsNums;
-      }
-      break;
-  
-    case 5:
-      if (special === false && lower === true && upper === false && num === false) {
-        charSet = charsLower;
-      }
-      break;
-  
-    case 6:
-      if (special === false && lower === true && upper === false && num === true) {
-        charSet = charsLower + charsNums;
-      }
-      break;
-  
-    case 7:
-      if (special === false && lower === true && upper === true && num === false) {
-        charSet = charsLower + charsUpper;
-      }
-      break;
-  
-    case 8:
-      if (special === false && lower === true && upper === true && num === true) {
-        charSet = charsLower + charsUpper + charsNums;
-      }
-      break;
-  
-    case 9:
-      if (special === true && lower === false && upper === false && num === false) {
-        charSet = charsSpec;
-      }
-      break;
-  
-    case 10:
-      if (special === true && lower === false && upper === false && num === true) {
-        charSet = charsSpec + charsNums;
-      }
-      break;
-  
-    case 11:
-      if (special === true && lower === false && upper === true && num === false) {
-        charSet = charsSpec + charsUpper;
-      }
-      break;
-  
-    case 12:
-      if (special === true && lower === false && upper === true && num === true) {
-        charSet = charsSpec + charsUpper + charsNums;
-      }
-      break;
-  
-    case 13:
-      if (special === true && lower === true && upper === false && num === false) {
-        charSet = charsSpec + charsLower;
-      }
-      break;
-  
-    case 14:
-      if (special === true && lower === true && upper === false && num === true) {
-        charSet = charsSpec + charsLower + charsNums;
-      }
-      break;
-  
-    case 15:
-      if (special === true && lower === true && upper === true && num === false) {
-        charSet = charsSpec + charsLower + charsUpper;
-      }
-      break;
-  
-    case 16:
-      if (special === true && lower === true && upper === true && num === true) {
-        charSet = charsSpec + charsLower + charsUpper + charsNums;
-      }
-      break;
-  
-    default:
-      // Handle the default case if needed.
-      break;
-  }
-  
-
-  if (charSet == ""){
-    alert("You must select at least one paramater");
-  }
-
-
-
-
-
-
 
 
   for (let i = 0; i < length; i++) {
@@ -177,23 +69,23 @@ function writePassword() {
   var passSpecial  = confirm("Should the password include special characters?");
 
 //check if to include lowercase chars
-  var passLower  = confirm("Should the password include lowercase characters?"); 
+  var passLower = confirm("shoul the password include lowercase characters?")
 
 //check if to include uppercase chars
-  var passUpper = confirm("Should the password include lowercase characters?");
+  var passUpper = confirm("Should the password include uppercase characters?");
 
 //check if to include numbers
   var passNum  = confirm("Should the password include numbers?");  
 
 
 // Relates all function vars to user inputs
-  const length = passLength;
+  var length = passLength;
 
 //converts to boolean
-  const special = passSpecial ? true : false;
-  const lower = passLower ? true : false; 
-  const upper = passUpper ? true : false;
-  const num = passNum ? true : false;
+  var special = passSpecial ? true : false;
+  var lower = passLower ? true : false; 
+  var upper = passUpper ? true : false;
+  var num = passNum ? true : false;
 
 
   
@@ -211,39 +103,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-/*
-var generateBtn = document.querySelector("#generate");
-
-function generatePassword(length) {
-  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let password = "";
-
-  for (let i = 0; i < length; i++) {
-    const chosenLen = Math.floor(Math.random() * chars.length);
-    password += chars.charAt(chosenLen);
-  }
-
-  return password;
-}
-
-function writePassword() {
-  var passwordText = document.querySelector("#password");
-  var passLength = prompt("How many characters should your password be? (8-128 characters)");
-
-  if (isNaN(passLength) || passLength <= 0) {
-    alert("Please enter a valid positive number for the password length.");
-    return;
-  }
-
-  var length = parseInt(passLength, 10);
-  var password = generatePassword(length);
-
-  passwordText.value = password;
-}
-
-generateBtn.addEventListener("click", writePassword);
-*/
